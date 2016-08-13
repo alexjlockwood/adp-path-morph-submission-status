b169515a7fced0eafcb61b757c209df8e2ff5755
Alex Lockwood <alockwood@google.com>
2016-08-13 18:48:35 -0400

    updated strings for demo

--- app/src/main/java/com/adp/example/checkmarkanimation/SquareView.java
+++ app/src/main/java/com/adp/example/checkmarkanimation/SquareView.java
@@ -12,21 +12,8 @@ public class SquareView extends View {
 
   @Override
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-    final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-    final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-    final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-
-    final int size;
-    if (widthMode == MeasureSpec.EXACTLY && widthSize > 0) {
-      size = widthSize;
-    } else if (heightMode == MeasureSpec.EXACTLY && heightSize > 0) {
-      size = heightSize;
-    } else {
-      size = widthSize < heightSize ? widthSize : heightSize;
-    }
-
-    final int finalMeasureSpec = MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY);
-    super.onMeasure(finalMeasureSpec, finalMeasureSpec);
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    int size = Math.min(getMeasuredWidth(), getMeasuredHeight());
+    setMeasuredDimension(size, size);
   }
 }
